efbf2981d037fd498733335ae5d63c91065bea82
MB-study <MB-study@192.168.1.156>
2020-11-14 16:26:33 +0800

    Spring

---
+++ src/main/java/com/sb/di/Teacher.java
@@ -0,0 +1,50 @@
+package com.sb.di;
+
+import java.util.Set;
+
+
+public class Teacher {
+    private  String name;
+    private Set<Student> students;
+    
+    
+    public Teacher() {
+        System.out.println("老師建構子1");
+    }
+
+    public Teacher(String name) {
+        this.name = name;
+         System.out.println("老師建構子2");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public void init(){
+        System.out.println("聘請老師");
+    }
+     public void destroy(){
+        System.out.println("老師退休");
+    }
+
+    public Set<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(Set<Student> students) {
+        this.students = students;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" + "name=" + name + ", students=" + students + '}';
+    }
+    
+    
+}
